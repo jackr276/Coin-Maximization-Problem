cd744c9d5cad1a8878df0cc0a21b95e986e09346
jackr276 <jack_m_robbins@me.com>
2024-07-15 17:16:59 -0400

    initialize program

---
+++ run.sh
@@ -0,0 +1,28 @@
+# Author: Jack Robbins
+#!/bin/bash
+
+# Simple runner script for c programs
+
+if [[ ! -d out ]]; then
+	mkdir out
+fi
+
+#Clean out the out dir
+rm -r out/*
+
+if [[ ! -f ./src/coin_problem.c ]]; then
+	echo "coin_problem.c does not exist"
+	exit 1
+fi
+
+
+# go heavy on the warnings
+gcc -Wall -Wextra ./src/count_problem.c -o ./out/coin_problem
+
+./out/coin_problem
+
+
+
+
+
+
