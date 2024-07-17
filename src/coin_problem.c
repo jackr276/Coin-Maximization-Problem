13c44ced6fa4334cc8e6e7387908b1821e92d317
jackr276 <jack_m_robbins@me.com>
2024-07-17 19:35:00 -0400

    Create coin_problem.c

---
+++ src/coin_problem.c
@@ -0,0 +1,21 @@
+/**
+ * Author: Jack Robbins
+ *
+ * This simple C program implements the Dynamic Programming Problem regarding coin choosing strategies
+ */
+
+#include <sys/types.h>
+
+struct Player{
+	u_int8_t total_money;
+};
+
+
+/**
+ * Program entry point
+ */
+int main(void){
+	
+
+	return 0;
+}
